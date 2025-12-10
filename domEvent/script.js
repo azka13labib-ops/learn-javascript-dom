ad0990685ae1974ed6792ce08b01108b908a8cfe
azka13labib-ops <azka13labib@gmail.com>
2025-12-10 22:07:17 +0700

    Add files via upload

---
+++ domEvent/script.js
@@ -0,0 +1,22 @@
+// const p3 = document.querySelector('.p3');
+// p3.onclick = ubahwarnap3;
+
+// function ubahwarnap2() {
+//   p2.style.backgroundColor = 'lightblue';
+// }
+// function ubahwarnap3() {
+//   p3.style.backgroundColor = 'lightblue';
+// }
+// const p2 = document.querySelector('.p2');
+// p2.onclick = ubahwarnap2;
+
+// const p4 = document.querySelector('#b p');
+// p4.addEventListener('click', function () {
+//   alert('ok');
+
+//   const ul = document.querySelector('section#b ul');
+//   const liBaru = document.createElement('li');
+//   const teksLiBaru = document.createTextNode('item baru');
+//   liBaru.appendChild(teksLiBaru);
+//   ul.appendChild(liBaru);
+// });
